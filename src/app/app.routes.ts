d9d0a7ffd05aa1198b1b2710e288144df285a279
Erick Batista <erick_batista07@yahoo.com>
2024-02-06 16:41:39 -0300

    chore: initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: '**', component: NotFoundComponent },
+    ],
+  },
+];
