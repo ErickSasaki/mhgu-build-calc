340a980a6fd6916c1a9e133ccbc038c9ba52de99
Erick Batista <erick_batista07@yahoo.com>
2024-02-20 10:34:48 -0300

    feat: weapon search filters, table header fixed

--- src/app/components/weapon-search-dialog/weapon-search-dialog.component.ts
+++ src/app/components/weapon-search-dialog/weapon-search-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, Inject } from '@angular/core';
+import { Component, Output, EventEmitter, Inject, ElementRef, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { WeaponSearchService } from '../../services/weapon-search.service';
 import { CommonModule } from '@angular/common';
@@ -8,9 +8,10 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { WeaponTypes } from '../../types/weapon-type';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { Observable, debounceTime, fromEvent } from 'rxjs';
 
-interface FilterParams {
-    searchName: string;
+interface Filters {
+    searchName?: string;
     final?: boolean;
 }
 
@@ -21,12 +22,15 @@ interface FilterParams {
     templateUrl: './weapon-search-dialog.component.html',
     styleUrl: './weapon-search-dialog.component.scss'
 })
-export class WeaponSearchDialogComponent {
+export class WeaponSearchDialogComponent implements OnInit, AfterViewInit {
     public tableColumns = ['id', 'name', 'type', 'attack', 'affinity'];
     public displayWeapons: WeaponModel[] = [];
     public loading = true;
 
     private weapons: WeaponModel[] = [];
+    private filters: Filters = { final: true };
+
+    @ViewChild('searchNameInput') private searchNameInput: ElementRef<HTMLInputElement> | undefined;
 
     constructor(
         private weaponSearchService: WeaponSearchService,
@@ -35,27 +39,68 @@ export class WeaponSearchDialogComponent {
     ) { }
 
     ngOnInit() {
-        this.weaponSearchService.searchWeapon({ type: this.data.weaponType })
-            .subscribe((response) => {
-                this.weapons = response as WeaponModel[];
-                this.displayWeapons = this.weapons;
-                this.loading = false;
-            });
+        this.getWeaponList();
+    }
+
+    ngAfterViewInit() {
+        this.setInputEvent();
     }
 
     public handleRowClick(event: WeaponModel) {
         this.dialogRef.close(event);
     }
 
-    public handleInputChange(event: Event) {
+    public handleSearchNameInputChange(event: Event) {
         const value = (event?.target as HTMLInputElement)?.value || '';
-
-        this.filter({ searchName: value })
+        this.updateSearchNameFilter(value);
     }
 
-    private filter(filterParams: FilterParams) {
-        this.displayWeapons = filterParams.searchName ?
-            this.weapons.filter((weapon) => weapon.name.toLowerCase().includes(filterParams.searchName)) :
+    public updateIsFinalFilter(checked: boolean) {
+        this.filters.final = checked;
+        this.updateFilters();
+    }
+
+    private getWeaponList() {
+        this.weaponSearchService.searchWeapon({ type: this.data.weaponType })
+            .subscribe((response) => {
+                this.weapons = response as WeaponModel[];
+                this.updateFilters();
+                this.loading = false;
+            });
+    }
+
+    private setInputEvent() {
+        const element = this.searchNameInput?.nativeElement;
+        const time = 500;
+
+        if (element) {
+            fromEvent(element, 'input')
+                .pipe(debounceTime(time))
+                .subscribe(this.handleSearchNameInputChange.bind(this));
+        }
+    }
+
+    private updateSearchNameFilter(searchName: string) {
+        this.filters.searchName = searchName;
+        this.updateFilters();
+    }
+
+    private updateFilters() {
+        const hasFilters = this.filters.searchName || this.filters.final;
+
+        this.displayWeapons = hasFilters ?
+            this.weapons.filter((weapon) => this.weaponMatchFilters(weapon, this.filters)) :
             this.weapons;
     }
+
+    private weaponMatchFilters(weapon: WeaponModel, filters: Filters): boolean {
+        const { searchName, final } = filters;
+        const lowerCaseWeaponName = weapon.name.toLocaleLowerCase();
+
+        const searchNameFilter = searchName ? lowerCaseWeaponName.includes(searchName) : true;
+        const isFinalFilter = final ? weapon.final === true : true;
+
+        return searchNameFilter && isFinalFilter;
+    }
+
 }
